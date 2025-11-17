9d7bf2453cb0ca6e9c752a5fbc30b0fa7a01a23c
Nguyễn Hữu Trường Sơn <edison001002019@gmail.com>
2025-11-17 23:05:28 +0700

    Enemy cho built2

--- scenes/player/states/attack.gd
+++ scenes/player/states/attack.gd
@@ -4,9 +4,9 @@ func _enter()->void:
 	timer = 0.2
 	obj.velocity.x=0
 	if obj.has_blade==true:
-		obj.get_node("Direction/HitArea2d/CollisionShape2D").disabled=false
+		obj.get_node("Direction/HitArea2D/CollisionShape2D").disabled=false
 func _exit()->void:
-	obj.get_node("Direction/HitArea2d/CollisionShape2D").disabled=true
+	obj.get_node("Direction/HitArea2D/CollisionShape2D").disabled=true
 func _update(delta:float)->void:
 	if update_timer(delta):
 		change_state(fsm.previous_state)
