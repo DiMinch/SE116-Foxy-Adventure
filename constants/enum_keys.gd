142dfea138cbf0c46513cecff76753284e3a2c1d
Võ Minh Kiệt <113196174+Vminhkiet@users.noreply.github.com>
2025-11-17 15:07:19 +0700

    Revert "[SE116][FT][ENEMY][PELICAN] complete pelican v1"

--- constants/enum_keys.gd
+++ constants/enum_keys.gd
@@ -1,10 +1,5 @@
 class_name EnumKeys
 
-enum EnemyType
-{
-	PELICAN
-}
-
 enum PlayerKeys {
 	HEALTH, 
 	MOVEMENT_SPEED, 
--- scripts/systems/enemies/pelican/bullet.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name BulletData
-
-@export var attack_speed: float = 1.0
-@export var attack_damage: int = 10
-@export var timelife: int = 10
