003c68d3ac3cd209f4e386be57043cdb4cad7c5f
Vminhkiet <minhkietvo6@gmail.com>
2025-11-21 02:54:16 +0700

    [SE116][FT][SKILLS][WEAPON] complete v1 weapon all tier 1 + passive + plus

--- scenes/characters/player/states/attack.gd
+++ scenes/characters/player/states/attack.gd
@@ -1,12 +1,16 @@
 extends PlayerState
+
 func _enter()->void:
-	obj.change_animation("attack")
+	obj.change_animation(ATTACK)
 	timer = 0.2
 	obj.velocity.x=0
-	if obj.has_blade==true:
-		obj.get_node("Direction/HitArea2D/CollisionShape2D").disabled=false
-func _exit()->void:
-	obj.get_node("Direction/HitArea2D/CollisionShape2D").disabled=true
+	timer = 0.3
+
+func _exit() -> void:
+	obj.is_attack = false
+	if obj.melee_hitbox:
+		obj.melee_hitbox.set_deferred("monitoring", false)
+
 func _update(delta:float)->void:
 	if update_timer(delta):
 		change_state(fsm.previous_state)
---
+++ scripts/systems/weapon/melee.gd
@@ -0,0 +1,5 @@
+extends WeaponData
+class_name MeleeData
+
+@export var passivebasic: Array = []
+@export var passiveplus: Array = []
